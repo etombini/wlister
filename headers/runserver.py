fb8822cf778954a42527b9144b3a2bc198fb8dbd
Elvis <elvis@mapom.me>
2014-01-15 18:10:02 +0100

    new file:   headers/runserver.py 	new file:   headers/templates/headers.html
    Simple Flask server that is proxied by apache + wlister

---
+++ headers/runserver.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request
+
+app = Flask('headers', template_folder='templates')
+
+
+@app.route('/<arg1>/<arg2>', methods=['GET', 'POST', 'HEAD', 'PUT', 'DELETE'])
+def page01(arg1, arg2):
+    #return str("<code>" + str(request.__dict__) + "</code>")
+    return render_template('headers.html',
+                           request=request,
+                           arg1=arg1,
+                           arg2=arg2)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
