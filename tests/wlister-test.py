6b1cd2834bf2b785e372541b5d57460e93a3390f
Elvis <elvis@mapom.me>
2014-01-24 18:35:58 +0100

    new tests

--- tests/wlister-test.py
+++ tests/wlister-test.py
@@ -15,3 +15,11 @@ class SimpleTest(unittest.TestCase):
     def test_proxy_ok(self):
         r = requests.get('http://localhost/')
         self.assertEqual(r.status_code, 200)
+
+    def test_01(self):
+        r = requests.get('http://localhost/abc')
+        self.assertEqual(r.status_code, 404)
+
+    def test_02(self):
+        r = requests.get('http://localhost/int/123')
+        self.assertEqual(r.status_code, 200)
