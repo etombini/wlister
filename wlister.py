5c31617dfce7516d0972951fbf457a292a82ae57
Elvis <elvis@mapom.me>
2014-02-03 18:29:42 +0100

    starting to work on request body

--- wlister.py
+++ wlister.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from mod_python import apache
+import json
 from wlrule import WLRule
 
 apache.wl_rules = None
@@ -8,7 +9,6 @@ apache.wl_config = None
 
 apache.log_error('WLISTER Imported', apache.APLOG_CRIT)
 
-import json
 
 def log(message, log_level=None):
     try:
@@ -18,8 +18,7 @@ def log(message, log_level=None):
         apache.log_error(message, apache.APLOG_CRIT)
 
 
-# ToDo: put init_wlister into this function !
-def init_wlister(request):
+def init_config(request):
     """
     Initialize wlister parameters from apache configuration
     Paramaters are:
@@ -32,11 +31,14 @@ def init_wlister(request):
     if apache.wl_config is not None:
         return True
     apache.wl_config = {}
-    apache.wl_rules = []
     options = request.get_options()
 
+    if 'wlister.conf' in options:
+        apache.wl_config['wlister.conf'] = options['wlister.conf']
+
     try:
-        apache.wl_config['wlister.log_prefix'] = options['wlister.log_prefix']
+        apache.wl_config['wlister.log_prefix'] = \
+            options['wlister.log_prefix'].strip()
     except:
         apache.wl_config['wlister.log_prefix'] = '[wlister]'
 
@@ -46,7 +48,8 @@ def init_wlister(request):
                 options['wlister.default_action']
         else:
             apache.wl_config['wlister.default_action'] = 'block'
-            log('unknown value for wlister.default_action (lock, pass, learn)')
+            log('unknown value for wlister.default_action ' +
+                '(block, pass, learn)')
             log('default action is ' +
                 apache.wl_config['wlister.default_action'])
     except:
@@ -55,69 +58,43 @@ def init_wlister(request):
         log('default action is ' +
             apache.wl_config['wlister.default_action'])
 
+    if 'wlister.max_post_read' in options:
+        apache.wl_config['wlister.max_post_read'] = \
+            options['wlister.max_post_read']
+    else:
+        apache.wl_config['wlister.max_post_read'] = 2048
+        log('default request body to be read set to ' +
+            str(apache.wl_config['wlister.max_post_read']))
+
+
+def init_rules():
+    if apache.wl_rules is not None:
+        return
+    apache.wl_rules = []
+    if 'wlister.conf' not in apache.wl_config:
+        log('No configuration file defined - ' +
+            'check [PythonOption wlister.conf filename] directive')
+        return
+    f = None
     try:
-        apache.wl_config['wlister.conf'] = options['wlister.conf']
+        f = open(apache.wl_config['wlister.conf'])
     except:
-        apache.wl_config['wlister.conf'] = None
-        log('wlister.conf is not set - can not analyze request')
-
-    if apache.wl_config['wlister.conf'] is not None:
-        f = None
-        try:
-            f = open(apache.wl_config['wlister.conf'])
-        except:
-            log('Can not open configuration file (wlister.conf) - ' +
-                str(apache.wl_config['wlister.conf']))
-        if f is not None:
-            d = None
-            try:
-                d = json.load(f)
-            except Exception as e:
-                log('Rules format is not json compliant - ' +
-                    str(apache.wl_config['wlister.conf']) +
-                    str(e))
-            if d is not None:
-                for r in d:
-                    apache.wl_rules.append(WLRule(r, log))
-    return True
-
-# Example of Rule format
-#rules = [
-#    {
-#        "id": "000000",
-#        "match":
-#        {
-#            "url": "^/\d+/\d+$",
-#            "protocol": "^HTTP/1\.1$",
-#            "method": "^GET$",
-#            "host": "^mapom.me$",
-#            "raw_parameters": "^$",
-#            "ip": "^127\.0\.0\.1$",
-#            "headers":
-#            {
-#                "Content-Type": "^application/x-www-form-urlencoded$",
-#                "Host": "^mapom.me$"
-#            }
-#        },
-#        "prerequisites":
-#        {
-#            "has_tag": ["tag01", "tag02"]
-#        },
-#        "actions_if_match":
-#        {
-#            "set_tag": ["tag03", "tag04"],
-#            "unset_tag": ["tag01"],
-#            "set_whitelisted": True
-#        },
-#        "actions_if_mismatch":
-#        {
-#            "set_tag": ["blah", "blih"]
-#        }
-#    }
-#]
+        log('Can not open configuration file (wlister.conf) - ' +
+            str(apache.wl_config['wlister.conf']))
+        return
+    d = None
+    try:
+        d = json.load(f)
+    except Exception as e:
+        log('Rules format is not json compliant - ' +
+            str(apache.wl_config['wlister.conf']) +
+            str(e))
+        return
+    for r in d:
+        apache.wl_rules.append(WLRule(r, log))
 
 
-def request_init(request):
+def init_request(request):
     request.wl_tags = set()
     request.wl_whitelisted = False
     request.wl_blacklisted = False
@@ -142,12 +119,36 @@ def request_init(request):
     # dealing with content-length if any
     if 'Content-Length' in request.headers_in:
         request.wl_tags.add('wl.has_body')
+        request.body_size = int(request.headers_in['Content-Length'])
+    else:
+        request.body_size = 0
+
+    max_size = apache.wl_config['wlister.max_post_read']
+    if request.body_size > max_size:
+        log('request body body length is higher than wlister.max_post_read (' +
+            max_size + ') - skipping body analyis')
+    elif (
+        request.body_size > 0 and
+        request.body_size < max_size
+    ):
+        try:
+            request.register_input_filter('wlister.pass_filter',
+                                          'wlister::input_filter')
+            request.add_input_filter('wlister.pass_filter')
+            request.body_raw = request.read(request.body_size)
+        except IOError:
+            log('reading request body failed - TimeOut may be reached - ' +
+                'request behavior is unknown')
+
+
+def init(request):
+    init_config(request)
+    init_rules()
+    init_request(request)
 
 
 def handler(req):
-    request_init(req)
-    init_wlister(req)
-
+    init(req)
     for rule in apache.wl_rules:
         rule.analyze(req)
         if req.wl_whitelisted:
@@ -176,25 +177,15 @@ def input_filter(filter):
 
     filter.req.log_error('--------------- START OF FILTER -----------------',
                          apache.APLOG_DEBUG)
-    filter.req.log_error('req.uuid ' + str(filter.req.uuid),
-                         apache.APLOG_DEBUG)
     # if there is a body content to forward...
     #if filter.req.body is not None:
     #    filter.write(filter.req.body)
     try:
-        r = filter.read()
-    except:
-        r = None
-    if r is not None:
-        if len(r) > filter.req.content_length:
-            filter.req.log_error('ALAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARM!!!!',
-                                 apache.APLOG_DEBUG)
-    if r is not None:
-        filter.write(r)
-    filter.req.log_error('filter.read() "' + str(r) + '"',
-                         apache.APLOG_DEBUG)
-    filter.flush()
-    filter.close()
+        filter.write(filter.req.body_raw)
+    except AttributeError:
+        log('request raw body is not defined')
+    finally:
+        filter.flush()
+        filter.close()
     filter.req.log_error('--------------- END OF FILTER -----------------',
                          apache.APLOG_DEBUG)
-    return apache.HTTP_NOT_FOUND
