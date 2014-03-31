e4719fd4d517d1f009a9c7a4fbf24ac538910a7c
Elvis <elvis@mapom.me>
2014-03-31 23:06:50 +0200

    lazy transformation done by WLRequest - may save some time

--- wlrequest.py
+++ wlrequest.py
@@ -2,6 +2,7 @@
 
 import os
 
+
 class WLRequest(object):
     def __init__(self, request, log=None, max_content_length=1024):
         self.request = request
@@ -10,16 +11,13 @@ class WLRequest(object):
         else:
             self.log = log
         self.max_content_length = max_content_length
-        self.content_length = 0
-        self.content = ''
+
+        self.lazy = ['parameters', 'content_length', 'content',
+                     'content_url_encoded']
+
         self.tags = set()
         self.whitelisted = False
         self.blacklisted = False
-
-        self.init_method()
-        self.init_parameters()
-        self.init_body_content()
-        self.init_body_content_type()
 
     def __repr__(self):
         return '<WLRequest %s>' % self.request.the_request
@@ -29,17 +27,15 @@ class WLRequest(object):
 
     def __getattr__(self, key):
         if key in ['uri', 'protocol', 'method', 'host', 'args']:
-            return getattr(self.request, key)
+            setattr(self, key, getattr(self.request, key))
+            return getattr(self, key)
+        elif key in self.lazy:
+            getattr(self, 'lazy_' + str(key))()
+            return getattr(self, key)
         else:
             raise AttributeError(key)
 
-    def init_method(self):
-        if self.request.method is None:
-            self.tags.add('wl.method.None')
-        else:
-            self.tags.add('wl.method.' + str(self.request.method).lower())
-
-    def init_parameters(self):
+    def lazy_parameters(self):
         # dealing with parameters
         if self.request.args is not None:
             self.tags.add('wl.has_parameters')
@@ -51,24 +47,25 @@ class WLRequest(object):
         else:
             self.parameters = None
 
-    def init_body_content(self):
-        # dealing with content-length if any
+    def lazy_content_length(self):
         if 'Content-Length' in self.request.headers_in:
-            self.tags.add('wl.has_body')
             self.content_length = \
                 int(self.request.headers_in['Content-Length'])
         else:
             self.content_length = 0
-            self.max_content_length = 0
-            return
 
+    def lazy_content(self):
+        if self.content_length == 0:
+            self.body_content = None
+            return
         if self.content_length > self.max_content_length:
             self.log('request body length (' +
                      str(self.request.headers_in['Content-Length']) +
-                     ') is greater than wlister.max_post_read (' +
-                     str(self.max_content_length) + ') - skipping body analyis')
+                     ' is greater than wlister.max_post_read (' +
+                     ' - skipping body analyis')
+            self.body_content = None
             return
-        elif self.content_length > 0:
+        if self.content_length > 0:
             try:
                 self.content = self.request.read(self.content_length)
                 current_path = os.path.dirname(os.path.abspath(__file__))
@@ -76,9 +73,6 @@ class WLRequest(object):
                                                    'wlister::input_filter',
                                                    current_path)
                 self.request.add_input_filter('wlister_pass_filter')
-                #self.content = self.request.read(self.content_length)
-                self.log('READING ' + str(self.content_length) + ' bytes')
-                self.log('CONTENT IS ' + str(self.content))
                 # Required to forward content when applying input filter
                 self.request.body = self.content
             except IOError:
@@ -86,32 +80,49 @@ class WLRequest(object):
                          ' - TimeOut may be reached - ' +
                          'request processing is unknown')
             except Exception as e:
-                self.log('WHAT IS THAT ? ' + str(e))
+                self.log('Unknown exception occurred - ' + str(e))
         else:
             self.tags.add('wl.bad_content_length')
 
-    def init_body_content_type(self):
-        if self.content_length == 0:
-            return
+    def lazy_content_url_encoded(self):
         if 'Content-Type' not in self.request.headers_in:
-            return
-        if self.request.headers_in['Content-Type'] == \
-                'application/x-www-form-urlencoded':
-            self.content_url_encoded = \
-                [arg.split('=', 1) for arg in self.content.split('&')]
-            for p in self.content_url_encoded:
-                if len(p) == 1:
-                    p.append('')
-            return
-        else:
             self.content_url_encoded = None
-        if self.request.headers_in['Content-Type'] == \
-                'application/json':
-            try:
-                import json
-                self.content_json = json.loads(self.content)
-            except:
-                self.log('can not load JSON content - json.loads() failed')
             return
-        else:
-            self.content_json = None
+        if self.request.headers_in['Content-Type'] != \
+                'application/x-www-form-urlencoded':
+            self.content_url_encoded = None
+            return
+        self.content_url_encoded = \
+            [arg.split('=', 1) for arg in self.content.split('&')]
+        for p in self.content_url_encoded:
+            if len(p) == 1:
+                p.append('')
+
+    def lazy_content_json(self):
+        pass
+
+#    def init_body_content_type(self):
+#        if self.content_length == 0:
+#            return
+#        if 'Content-Type' not in self.request.headers_in:
+#            return
+#        if self.request.headers_in['Content-Type'] == \
+#                'application/x-www-form-urlencoded':
+#            self.content_url_encoded = \
+#                [arg.split('=', 1) for arg in self.content.split('&')]
+#            for p in self.content_url_encoded:
+#                if len(p) == 1:
+#                    p.append('')
+#            return
+#        else:
+#            self.content_url_encoded = None
+#        if self.request.headers_in['Content-Type'] == \
+#                'application/json':
+#            try:
+#                import json
+#                self.content_json = json.loads(self.content)
+#            except:
+#                self.log('can not load JSON content - json.loads() failed')
+#            return
+#        else:
+#            self.content_json = None
